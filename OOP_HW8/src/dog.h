71bc2556e4fe7c348f489f1570149c41c3d8b4dc
楊明哲 <t109590009@ntut.edu.tw>
2022-01-20 00:49:12 +0800

    HW8

---
+++ OOP_HW8/src/dog.h
@@ -0,0 +1,63 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdexcept>
+#include "pet.h"
+class Dog : public Pet {
+    public:
+        Dog(){}
+        Dog(std::string name,double weight):Pet("DOG",name,weight){
+            if(weight < 10 || weight > 50){
+                throw std::range_error("undefined.");
+            }
+            ++_amount;
+        }
+        Dog(Dog const &dog) : Pet(dog){
+            ++_amount;
+        }
+        ~Dog() {
+            _amount--;
+        }
+        static int amount() {
+            return _amount;
+        }
+
+        void feed() override {
+            addweight(0.8);
+            if(weight() > 50){
+                throw std::out_of_range("undefined.");
+            }
+        }
+    private:
+        static int _amount;
+};
+int Dog::_amount = 0;
+#endif
+
+
+// #ifndef DOG_H
+// #define DOG_H
+
+// #include <stdexcept>
+// #include <string>
+
+// #include "pet.h"
+
+
+// class Dog : public Pet {
+// public:
+
+//   /*  add and modify your code from assignment 8  */
+
+//   static int amount() {
+//     return _amount;
+//   }
+
+// private:
+//   static int _amount;
+
+// };
+
+// int Dog::_amount = 0;
+
+
+// #endif /* end of include guard: DOG_H */
